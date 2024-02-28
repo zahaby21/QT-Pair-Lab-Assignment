e5febd60502669f57bfdd567ad1a93659135ec94
Zahaby <Zahaby@aucegypt.edu>
2024-02-28 20:31:20 +0200

    Ahmed's Changes 1

---
+++ loginwindow.cpp
@@ -0,0 +1,16 @@
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+
+LoginWindow::LoginWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::LoginWindow)
+{
+    ui->setupUi(this);
+    ui->errorlabel->setVisible(false);
+}
+
+
+LoginWindow::~LoginWindow()
+{
+    delete ui;
+}
